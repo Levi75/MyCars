a03d0ca38f1ea0600df240fc53c63e022d84da0b
tyoma <tyoma1998@mail.ru>
2020-07-10 18:23:48 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import CreateCar from "./containers/createCar/CreateCar";
 import CreateUser from "./containers/createUser/CreateUser";
 
 function App() {
   return (
     <>
-      <p>
-        <Link to="/сreate-сar">Create Car </Link>
-      </p>
-      <p>
-        <Link to="/сreate-user">Create User</Link>
-      </p>
-      <Router>
+      <div>
+        <p>
+          <Link to="/сreate-сar">Create Car </Link>
+        </p>
+        <p>
+          <Link to="/сreate-user">Create User</Link>
+        </p>
+      </div>
+      <div>
         <Switch>
           <Route path="/сreate-сar" component={CreateCar} />
           <Route path="/сreate-user" component={CreateUser} />
         </Switch>
-      </Router>
+      </div>
     </>
   );
 }
