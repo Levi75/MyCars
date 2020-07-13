7c7cc96d17a78d62541e16b0b283c0ab762f65d7
Nikitin Aleksandr <anikitin.exceedteam@gmail.com>
2020-07-13 17:02:00 +0300

    [front] add garage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import { Route, Link, Switch } from "react-router-dom";
 import CreateCar from "./containers/createCar/CreateCar";
 import CreateUser from "./containers/createUser/CreateUser";
-import DisplayCars from "./containers/displayCars/DisplayCars"
-
+import DisplayCars from "./containers/displayCars/DisplayCars";
+import Garage from "./containers/garage/Garage";
 function App() {
   return (
     <>
@@ -18,12 +18,16 @@ function App() {
         <p>
           <Link to="/display-car">Display Cars</Link>
         </p>
+        <p>
+          <Link to="/garage">Garage</Link>
+        </p>
       </div>
       <div>
         <Switch>
           <Route path="/сreate-сar" component={CreateCar} />
           <Route path="/сreate-user" component={CreateUser} />
           <Route path="/display-car" component={DisplayCars} />
+          <Route path="/garage" component={Garage} />
         </Switch>
       </div>
     </>
