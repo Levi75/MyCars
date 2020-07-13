cc333f520a35c1be3535dbe6a47d1218ae4f53f6
Nikitin Aleksandr <anikitin.exceedteam@gmail.com>
2020-07-13 10:44:02 +0300

    fix create car

--- src/containers/createCar/CreateCar.js
+++ src/containers/createCar/CreateCar.js
@@ -11,7 +11,7 @@ export default function CreateCar() {
   return (
     <div className={Styles.Car}>
       <div className={Styles.Car_container}>
-        Создание машины
+        
         <Form
           onSubmit={onSubmit}
           initialValues={{}}
