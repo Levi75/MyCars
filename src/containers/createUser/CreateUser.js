10d4d431487d42b6a5f8da75cfd68f8c3a9584c9
tyoma <tyoma1998@mail.ru>
2020-07-14 17:38:09 +0300

    add new style in page create user

--- src/containers/createUser/CreateUser.js
+++ src/containers/createUser/CreateUser.js
@@ -4,6 +4,7 @@ import BootstrapForm from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Form, Field } from "react-final-form";
 import axios from "axios";
+import { MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from "mdbreact";
 
 export default function CreateUser() {
   const postUser = async (value) => {
@@ -23,50 +24,65 @@ export default function CreateUser() {
   };
   return (
     <div className={Styles.User}>
-      <div className={Styles.User_container}>
-        Создание пользователей
-        <Form
-          onSubmit={onSubmit}
-          initialValues={{}}
-          render={({ handleSubmit }) => (
-            <form onSubmit={handleSubmit}>
-              <BootstrapForm.Group controlId="formBasicEmail">
-                <BootstrapForm.Label>Email </BootstrapForm.Label>
-                <Field name="email">
-                  {({ input }) => (
-                    <BootstrapForm.Control
-                      name="email"
-                      type="email"
-                      placeholder="Enter email"
-                      onChange={(date) => {
-                        input.onChange(date);
-                      }}
-                    />
-                  )}
-                </Field>
-              </BootstrapForm.Group>
-              <BootstrapForm.Group controlId="formBasicPassword">
-                <BootstrapForm.Label>Name</BootstrapForm.Label>
-                <Field name="name">
-                  {({ input }) => (
-                    <BootstrapForm.Control
-                      name="name"
-                      type="text"
-                      placeholder="Name"
-                      onChange={(date) => {
-                        input.onChange(date);
-                      }}
-                    />
-                  )}
-                </Field>
-              </BootstrapForm.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </form>
-          )}
-        />
-      </div>
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{}}
+        render={({ handleSubmit }) => (
+          <form className={Styles.FormUser} onSubmit={handleSubmit}>
+            <MDBCol md="6">
+              <MDBCard>
+                <MDBCardBody>
+                  <p className="h4 text-center py-4">Sign up</p>
+                  <div className="grey-text">
+                    <Field name="email">
+                      {({ input }) => (
+                        <MDBInput
+                          label="Email"
+                          icon="envelope"
+                          group
+                          validate
+                          name="email"
+                          type="email"
+                          error="wrong"
+                          success="right"
+                          placeholder="Enter email"
+                          onChange={(date) => {
+                            input.onChange(date);
+                          }}
+                        />
+                      )}
+                    </Field>
+
+                    <Field name="name">
+                      {({ input }) => (
+                        <MDBInput
+                          label="Name"
+                          icon="user"
+                          validate
+                          error="wrong"
+                          success="right"
+                          group
+                          name="name"
+                          type="text"
+                          placeholder="Name"
+                          onChange={(date) => {
+                            input.onChange(date);
+                          }}
+                        />
+                      )}
+                    </Field>
+                  </div>
+                  <div className="text-center py-4 mt-3">
+                    <MDBBtn color="cyan" type="submit">
+                      Register
+                    </MDBBtn>
+                  </div>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </form>
+        )}
+      />
     </div>
   );
 }
