efc2cc107a196cf0af808045dc4d5105a27d4d00
tyoma <tyoma1998@mail.ru>
2020-07-17 10:41:18 +0300

    fix bags style in change car

--- src/containers/ChangeCar/ChangeCar.js
+++ src/containers/ChangeCar/ChangeCar.js
@@ -11,8 +11,6 @@ export default function ChangeCar() {
   const [isLoading, setIsLoading] = React.useState(false);
   const idCar = useParams().id;
   let year;
-  console.log(car);
-  console.log(car.year);
 
   if (car.year) {
     year = car.year.split("-")[0];
@@ -119,175 +117,177 @@ export default function ChangeCar() {
     return <Spinner />;
   }
   return (
-    <div className="col-4 offset-4 p-3">
-      <Form
-        onSubmit={onSubmit}
-        initialValues={{}}
-        render={({ handleSubmit }) => (
-          <form onSubmit={handleSubmit} className={Styles.Form}>
-            <div className={Styles.Form__Create_User}>
-              <MDBCard>
-                <MDBCardBody>
-                  <p className="h4 text-center py-4">Редактирование машины</p>
-                  <div className="grey-text">
-                    <Field name="name">
-                      {({ input }) => (
-                        <MDBInput
-                          label="Имя"
-                          icon="user"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="name"
-                          type="text"
-                          valueDefault={car.name}
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        />
-                      )}
-                    </Field>
-                    <Field name="price">
-                      {({ input }) => (
-                        <MDBInput
-                          label="Цена"
-                          icon="dollar-sign"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="price"
-                          type="number"
-                          valueDefault={car.price}
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        />
-                      )}
-                    </Field>
-                    <Field name="year">
-                      {({ input }) => (
-                        <select
-                          className="browser-default custom-select"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="year"
-                          type="text"
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        >
-                          <option>{year}</option>
-                          {years.map((year) => {
-                            return (
-                              <option value={`${year}-01-01`}>{year}</option>
-                            );
-                          })}
-                        </select>
-                      )}
-                    </Field>
+    <div className={Styles.ChangeCar}>
+      <div className="col-4 offset-4 p-3">
+        <Form
+          onSubmit={onSubmit}
+          initialValues={{}}
+          render={({ handleSubmit }) => (
+            <form onSubmit={handleSubmit} className={Styles.Form}>
+              <div className={Styles.Form__Create_User}>
+                <MDBCard>
+                  <MDBCardBody>
+                    <p className="h4 text-center py-4">Редактирование машины</p>
+                    <div className="grey-text">
+                      <Field name="name">
+                        {({ input }) => (
+                          <MDBInput
+                            label="Имя"
+                            icon="user"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="name"
+                            type="text"
+                            valueDefault={car.name}
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          />
+                        )}
+                      </Field>
+                      <Field name="price">
+                        {({ input }) => (
+                          <MDBInput
+                            label="Цена"
+                            icon="dollar-sign"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="price"
+                            type="number"
+                            valueDefault={car.price}
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          />
+                        )}
+                      </Field>
+                      <Field name="year">
+                        {({ input }) => (
+                          <select
+                            className="browser-default custom-select"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="year"
+                            type="text"
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          >
+                            <option>{year}</option>
+                            {years.map((year) => {
+                              return (
+                                <option value={`${year}-01-01`}>{year}</option>
+                              );
+                            })}
+                          </select>
+                        )}
+                      </Field>
 
-                    <Field name="brand">
-                      {({ input }) => (
-                        <MDBInput
-                          label="Марка"
-                          icon="bimobject"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="brand"
-                          type="text"
-                          valueDefault={car.brand}
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        />
-                      )}
-                    </Field>
-                    <Field name="model">
-                      {({ input }) => (
-                        <MDBInput
-                          label="Модель"
-                          icon="car"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="model"
-                          type="text"
-                          placeholder="Имя"
-                          valueDefault={car.model}
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        />
-                      )}
-                    </Field>
-                    <Field name="boxType">
-                      {({ input }) => (
-                        <select
-                          className="browser-default custom-select"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="boxType"
-                          type="text"
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        >
-                          <option value={car.boxtype}>{boxTypeRus}</option>
-                          <option value={"Manual transmission"}>
-                            Механическая коробка
-                          </option>
-                          <option value={"Automatic transmission"}>
-                            Автоматическая коробка
-                          </option>
-                          <option value={"Robotic gearbox"}>
-                            Роботизированная коробк
-                          </option>
-                          <option value={"Variable transmission"}>
-                            Вариативная коробка
-                          </option>
-                        </select>
-                      )}
-                    </Field>
-                    <Field name="engineCapacity">
-                      {({ input }) => (
-                        <MDBInput
-                          label="Мощность двигателя"
-                          icon="chart-line"
-                          validate
-                          error="wrong"
-                          success="right"
-                          group
-                          name="engineCapacity"
-                          type="number"
-                          placeholder="Имя"
-                          valueDefault={car.enginecapacity}
-                          onChange={(date) => {
-                            input.onChange(date);
-                          }}
-                        />
-                      )}
-                    </Field>
-                  </div>
-                  <div className="text-center py-4 mt-3">
-                    <MDBBtn color="blue darken-3" type="submit">
-                      Редактировать
-                    </MDBBtn>
-                  </div>
-                </MDBCardBody>
-              </MDBCard>
-            </div>
-          </form>
-        )}
-      />
+                      <Field name="brand">
+                        {({ input }) => (
+                          <MDBInput
+                            label="Марка"
+                            icon="bimobject"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="brand"
+                            type="text"
+                            valueDefault={car.brand}
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          />
+                        )}
+                      </Field>
+                      <Field name="model">
+                        {({ input }) => (
+                          <MDBInput
+                            label="Модель"
+                            icon="car"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="model"
+                            type="text"
+                            placeholder="Имя"
+                            valueDefault={car.model}
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          />
+                        )}
+                      </Field>
+                      <Field name="boxType">
+                        {({ input }) => (
+                          <select
+                            className="browser-default custom-select"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="boxType"
+                            type="text"
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          >
+                            <option value={car.boxtype}>{boxTypeRus}</option>
+                            <option value={"Manual transmission"}>
+                              Механическая коробка
+                            </option>
+                            <option value={"Automatic transmission"}>
+                              Автоматическая коробка
+                            </option>
+                            <option value={"Robotic gearbox"}>
+                              Роботизированная коробк
+                            </option>
+                            <option value={"Variable transmission"}>
+                              Вариативная коробка
+                            </option>
+                          </select>
+                        )}
+                      </Field>
+                      <Field name="engineCapacity">
+                        {({ input }) => (
+                          <MDBInput
+                            label="Мощность двигателя"
+                            icon="chart-line"
+                            validate
+                            error="wrong"
+                            success="right"
+                            group
+                            name="engineCapacity"
+                            type="number"
+                            placeholder="Имя"
+                            valueDefault={car.enginecapacity}
+                            onChange={(date) => {
+                              input.onChange(date);
+                            }}
+                          />
+                        )}
+                      </Field>
+                    </div>
+                    <div className="text-center py-4 mt-3">
+                      <MDBBtn color="blue darken-3" type="submit">
+                        Редактировать
+                      </MDBBtn>
+                    </div>
+                  </MDBCardBody>
+                </MDBCard>
+              </div>
+            </form>
+          )}
+        />
+      </div>
     </div>
   );
 }
